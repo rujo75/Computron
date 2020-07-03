194b8c71ff377fbd7dfb0404ed537ed408d221c8
Dalibor Rusmir <drusmir@tpg.com.au>
2020-07-03 17:16:05 +1000

    Updated settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,20 +1,111 @@
 <template>
-  <div>Settings</div>
+  <div>
+    <dx-toolbar class="dx-theme-border-color">
+      <dx-item :options="saveNavButtonOptions" location="before" widget="dxButton" />
+      <dx-item :options="cancelNavButtonOptions" location="before" widget="dxButton" />
+    </dx-toolbar>
+    <div class="widget-container">
+      <dx-tab-panel
+        :items="tabsData"
+        :animation-enabled="true"
+        :swipe-enabled="false"
+        :deferRendering="false"
+        item-title-template="title"
+        class="tab-panel"
+      >
+        <template #title="{ data }">
+          <div>
+            <span>{{ data.title }}</span>
+            <i class="fas fa-exclamation-circle red" v-if="!data.isValid"></i>
+          </div>
+        </template>
+        <div class="tab" slot="GeneralTab"></div>
+      </dx-tab-panel>
+    </div>
+  </div>
 </template>
 
 <script>
+import { DxToolbar, DxItem } from "devextreme-vue/toolbar";
+import DxTabPanel from "devextreme-vue/tab-panel";
 import { mapGetters } from "vuex";
 
 export default {
   name: "settings",
+  components: {
+    DxToolbar,
+    DxItem,
+    DxTabPanel
+  },
   data() {
-    return {};
+    return {
+      formData: null,
+      formOriginalData: null,
+      tabsData: [
+        {
+          title: "General",
+          template: "GeneralTab",
+          isValid: true
+        }
+      ],
+      saveNavButtonOptions: {
+        icon: "fas fa-save",
+        focusStateEnabled: false,
+        stylingMode: "text",
+        text: "Save",
+        onClick: this.onSaveClick.bind(this)
+      },
+      cancelNavButtonOptions: {
+        icon: "fas fa-times",
+        focusStateEnabled: false,
+        stylingMode: "text",
+        text: "Cancel",
+        onClick: () => {
+          this.$router.back();
+        }
+      }
+    };
   },
   computed: {
     ...mapGetters(["getCurrentPath"])
+  },
+  methods: {
+    onSaveClick() {}
+  },
+  created() {
+    //console.log("created");
+    //this.loadFormData();
   },
   mounted() {
     //console.log("mounted");
   }
 };
 </script>
+
+<style scoped>
+.dx-toolbar {
+  /* background-color: rgba(191, 191, 191, 0.15); */
+  padding: 5px 10px;
+  border-bottom: 1px solid;
+}
+
+.tab-panel {
+  margin: 10px;
+  height: calc(100vh - 186px);
+}
+
+.tab {
+  padding: 15px 15px;
+}
+
+.form {
+  height: calc(100vh - 257px);
+}
+
+.red {
+  color: #d9534f;
+  font-size: 18px;
+  margin-left: 10px;
+  vertical-align: -2px;
+}
+</style>
